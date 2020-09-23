0c7cb552fb5e7737bc9613d52014f02cc7437be7
strawnp <42393469+strawnp@users.noreply.github.com>
2020-09-23 10:58:45 -0700

    Add population lab program

---
+++ unit1/population.c
@@ -0,0 +1,39 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt for start size
+    int startSize;
+    do
+    {
+        startSize = get_int("Start size: ");
+    }
+    while (startSize < 9);
+
+    // Prompt for end size
+    int endSize;
+    do
+    {
+        endSize = get_int("End size: ");
+    }
+    while (endSize < startSize);
+
+    // TODO: Calculate number of years until we reach threshold
+    int numYears = 0;
+
+    while (startSize < endSize)
+    {
+        int newLlamas = startSize / 3;
+        int ripLlamas = startSize / 4;
+        // startSize = startSize + newLlamas - ripLlamas;
+        startSize += newLlamas - ripLlamas;                     // compound assignment
+        // numYears = numYears + 1;
+        // numYears += 1;
+        numYears++;                                             // increment operator
+        // printf("DEBUG - Updated population: %d\n", startSize);
+    }
+
+    // Print number of years
+    printf("Years: %d\n", numYears);
+}
