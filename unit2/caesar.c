17d54b57f96135e59333d82773c074fed9500fdb
Peter Strawn <peter.strawn@jesuithighschool.org>
2020-12-03 17:14:18 +0000

    Add starter code for caesar

---
+++ unit2/caesar.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    // TODO: validate command-line arguments
+
+    string plaintext = get_string("plaintext: ");
+    printf("ciphertext: ");
+
+    // TODO: convert command-line argument to integer for the letter shift
+
+    // TODO: iterate over plaintext
+
+        // TODO: determine whether or not a character should be shifted
+
+            // TODO: shift character by the user-provided number maintaining capitalization
+
+        // TODO: print character
+
+    printf("\n");
+}
